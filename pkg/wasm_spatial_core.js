cf5bd7b1412457d0fe1fe3858bcf5bd7548c5cc4
reed-soul <reed-soul@users.noreply.github.com>
2026-06-30 17:43:53 +0000

    deploy: 0a82aca45e1196e3fdccb53efe74f1287729ec5a

--- pkg/wasm_spatial_core.js
+++ pkg/wasm_spatial_core.js
@@ -8096,7 +8096,7 @@ function __wbg_get_imports() {
                     const a = state0.a;
                     state0.a = 0;
                     try {
-                        return __wasm_bindgen_func_elem_3666(a, state0.b, arg0, arg1);
+                        return __wasm_bindgen_func_elem_3675(a, state0.b, arg0, arg1);
                     } finally {
                         state0.a = a;
                     }
@@ -8234,12 +8234,12 @@ function __wbg_get_imports() {
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 11, ret: Unit, inner_ret: Some(Unit) }, mutable: false }) -> Externref`.
-            const ret = makeClosure(arg0, arg1, __wasm_bindgen_func_elem_1109);
+            const ret = makeClosure(arg0, arg1, __wasm_bindgen_func_elem_1117);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 225, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_3660);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_3669);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000003: function(arg0) {
@@ -8266,14 +8266,14 @@ function __wbg_get_imports() {
     };
 }
 
-function __wasm_bindgen_func_elem_1109(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_1109(arg0, arg1, addHeapObject(arg2));
+function __wasm_bindgen_func_elem_1117(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_1117(arg0, arg1, addHeapObject(arg2));
 }
 
-function __wasm_bindgen_func_elem_3660(arg0, arg1, arg2) {
+function __wasm_bindgen_func_elem_3669(arg0, arg1, arg2) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.__wasm_bindgen_func_elem_3660(retptr, arg0, arg1, addHeapObject(arg2));
+        wasm.__wasm_bindgen_func_elem_3669(retptr, arg0, arg1, addHeapObject(arg2));
         var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
         var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         if (r1) {
@@ -8284,8 +8284,8 @@ function __wasm_bindgen_func_elem_3660(arg0, arg1, arg2) {
     }
 }
 
-function __wasm_bindgen_func_elem_3666(arg0, arg1, arg2, arg3) {
-    wasm.__wasm_bindgen_func_elem_3666(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
+function __wasm_bindgen_func_elem_3675(arg0, arg1, arg2, arg3) {
+    wasm.__wasm_bindgen_func_elem_3675(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
 }
 
 const Cesium3DTileFinalization = (typeof FinalizationRegistry === 'undefined')
